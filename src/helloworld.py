c42edc06ac97a909a255482fc04b45b54e5bbf41
Your Name <bastihd2006@gmail.com>
2024-04-25 19:41:57 +0200

    first commit

---
+++ src/helloworld.py
@@ -0,0 +1,18 @@
+from termcolor import colored
+
+print("\n\n")
+
+print(colored("      H", "red", attrs=["bold"]),
+      colored("e", "light_red", attrs=["bold"]),
+      colored("l", "yellow", attrs=["bold"]),
+      colored("l", "green", attrs=["bold"]),
+      colored("o", "blue", attrs=["bold"]),
+      colored("  ", "white", attrs=["bold"]),
+      colored("W", "cyan", attrs=["bold"]),
+      colored("o", "magenta", attrs=["bold"]),
+      colored("r", "light_red", attrs=["bold"]),
+      colored("l", "yellow", attrs=["bold"]),
+      colored("d", "green", attrs=["bold"]),
+      colored("!", "blue", attrs=["bold"]))
+
+print("\n\n")
